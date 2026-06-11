7b7009fa432d2867767cc1c7f7497f31025074d2
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-11 23:28:00 +0530

    creating the board

---
+++ breakOut.js
@@ -0,0 +1,13 @@
+//board
+
+let board;
+let boardWidth = 500;
+let boardHeight = 500;
+let context;
+
+window.onload = function(){
+    board = document.getElementById('board')
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext('2d');//Used fot drawing in the board
+}
