abc203e96b0f7769b0e3f264c5e7cc2d0d4083e2
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-12 22:09:21 +0530

    player

--- breakOut.js
+++ breakOut.js
@@ -5,6 +5,19 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
+
+// Player
+
+let playerHeight = 10;
+let playerWidth = 80;
+
+let player = {
+    x : boardWidth/2 - playerWidth / 2,
+    y : boardHeight - playerHeight - 5,
+    width : playerWidth,
+    height : playerHeight
+}
+
 window.onload = function(){
     board = document.getElementById('board')
     board.height = boardHeight;
