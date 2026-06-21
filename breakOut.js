b80105ad2ad3a9f3bdbe7d2b5d397696e275b843
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-21 23:44:52 +0530

    brick breaking

--- breakOut.js
+++ breakOut.js
@@ -119,3 +119,4 @@ function detectCollision(a, b){
 function topCollision(ball, block){
     return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
 }
+console.log('brick breaker')
